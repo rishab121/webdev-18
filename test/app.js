d4e20cfb888b32b01efa8f51ed79b72710e54389
rishab121 <rishabmalik121@gmail.com>
2018-04-30 19:50:14 -0400

    mongo config

--- test/app.js
+++ test/app.js
@@ -6,14 +6,15 @@ app.post("/api/test", createMessage);
 app.delete("/api/test/:id", deleteMessage);
 
 var connectionString = 'mongodb://127.0.0.1:27017/webdev_summer2_2017'; // for local
-// if(process.env.MLAB_USERNAME_WEBDEV) { // check if running remotely
-//     var username = process.env.MLAB_USERNAME_WEBDEV; // get from environment
-//     var password = process.env.MLAB_PASSWORD_WEBDEV;
-//     connectionString = 'mongodb://' + username + ':' + password;
-//     connectionString += '@ds157268.mlab.com:57268/heroku_nh37fqq4'; // user yours
-// }
+if(process.env.MLAB_USERNAME) { // check if running remotely
+    var username = process.env.MLAB_USERNAME; // get from environment
+    var password = process.env.MLAB_PASSWORD;
+    connectionString = 'mongodb://' + username + ':' + password;
+    connectionString += '@ds161529.mlab.com:61529/heroku_8zxvhpd0'; // user yours
+}
 // Replace "@ds157268.mlab.com:57268/heroku_nh37fqq4"
 // above with your own URL given to you by mLab
+// mongodb://<dbuser>:<dbpassword>@ds161529.mlab.com:61529/heroku_8zxvhpd0
 
 var mongoose = require("mongoose");
 mongoose.connect(connectionString);
